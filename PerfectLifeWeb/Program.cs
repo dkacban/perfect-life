f961ac1e2c5c58ad15c1a6330c1797d3e0d7f7f5
dkacban <darek.kacban@gmail.com>
2017-04-18 22:55:29 +0200

    set port to 6000

--- PerfectLifeWeb/Program.cs
+++ PerfectLifeWeb/Program.cs
@@ -17,6 +17,7 @@ namespace PerfectLifeWebService
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
+                .UseUrls("http://localhost:6000")
                 .Build();
 
             host.Run();
